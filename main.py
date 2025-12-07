ea59a3bad5affd8911987007c53894e27cc84c0b
raxdiusid <raxdiusid@gmail.com>
2025-12-08 01:52:38 +0700

    Bug on the last function where the volume grows anomalious, will fix it later.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import math
 
 class Function:
-    def __init__(self, f, a, b):
+    def __init__(self, f, a, b, n):
         self.f = f
         self.a = a
         self.b = b
+        self.dx = (b-a)/n
 
     def x_is_in_range(self, x):
         return self.a <= x <= self.b
@@ -19,18 +20,22 @@ class Disk:
         self.real_vol = real_vol
 
         self.acc_vol = 0
+        self.volume_each_function = []
 
         self.run()
 
     def run(self):
-        for i in range(self.n):
-            xi = self.a + i*self.dx
-            for f in self.f:
-                if not f.x_is_in_range(xi):
-                    continue
-                print(f"x_{i} = {xi} | f({xi}) = {f.f(xi)}")
-                self.acc_vol += math.pi*(f.f(xi))**2 * self.dx
-                break
+        for f in self.f:
+            vol = 0
+            for i in range(self.n):
+                xi = f.a + i*f.dx
+                vol += math.pi * (f.f(xi))**2 * f.dx
+                print(f"x_i = {xi} | f(x_i) = {f.f(xi)} | {vol} mL")
+            self.volume_each_function.append(vol)
+
+        print(self.volume_each_function)
+        self.acc_vol = sum(self.volume_each_function)
+                
         print("="*5)
         print(f"n: {self.n}")
         print(f"dx: {self.dx}")
@@ -39,10 +44,17 @@ class Disk:
         print(f"Galat: {(abs(self.real_vol-self.acc_vol)/self.real_vol)*100}%")
                     
 
-for i in range(6):
-    disk = Disk(0, 19.5, 10*(i+1), [
-        Function(lambda x: 1/math.sqrt(3) * math.sqrt(x) + 2.5, 0, 3),
-        Function(lambda x: 4/49 * x**2 - 52/49 * x + 583/98, 3, 10),
-        Function(lambda x: 3.5, 10, 11.5),
-        Function(lambda x: -0.28125*x+6.734375, 11.5, 19.5)
+# for i in range(1):
+#     disk = Disk(0, 19.5, 10*(i+1), [
+#         Function(lambda x: 1/math.sqrt(3) * math.sqrt(x) + 2.5, 0, 3),
+#         Function(lambda x: 4/49 * x**2 - 52/49 * x + 583/98, 3, 10),
+#         Function(lambda x: 3.5, 10, 11.5),
+#         Function(lambda x: -0.28125*x+6.734375, 11.5, 19.5)
+#     ], 350)
+
+disk = Disk(0, 19.5, 10, [
+        Function(lambda x: 1/math.sqrt(3) * math.sqrt(x) + 2.5, 0, 3, 10),
+        Function(lambda x: 4/49 * x**2 - 52/49 * x + 583/98, 3, 10, 10),
+        Function(lambda x: 3.5, 10, 11.5, 10),
+        Function(lambda x: -0.28125*x + 6.734375, 11.5, 19.5, 10)
     ], 350)
